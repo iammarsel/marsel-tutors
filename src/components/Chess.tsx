f4976ac63abbf9eafa204594484af88ed3d6a215
Marsel Fetlyaev <fetmarsel2002@gmail.com>
2023-12-30 18:17:59 -0500

    navbar and text settings in chess

--- src/components/Chess.tsx
+++ src/components/Chess.tsx
@@ -1,30 +1,22 @@
 import React from 'react';
 import chess_banner from '../assets/chess_banner.png';
-import logo from '../assets/logo.png';
+import Navbar from './Navbar';
 const Chess: React.FC = () => {
   return (
     <>
-    <nav className="flex items-center justify-between p-4">
-      <div className="flex items-center">
-        <img src={ logo } alt="Logo" className="h-8 mr-4" />
-        <span className="text-xl font-bold">marseltutors</span>
-      </div>
-      <div>
-        <button className="text-base font-semibold hover:underline">Register</button>
-      </div>
-    </nav>
-    <div className="flex flex-col">
+    <Navbar />
+    <div className="flex flex-col mt-16">
       {/* Full-width header image */}
       <div style={{maxHeight: '60vh'}}className="w-full overflow-hidden">
         <img src={ chess_banner } alt="Chess Banner" className="w-full h-auto" />
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 my-8">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 my-8 mx-4">
         <div className="bg-yellow-200 p-4 rounded-lg shadow-md">
           <h2 className="font-semibold text-lg">BEGINNER</h2>
           <ul className="list-disc ml-4 mt-2">
             <li>basics of piece movement and checkmate</li>
             <li>puzzles and online games</li>
-            <li>rating ~ 1000+</li>
+            <li>rating goal: 1000+</li>
           </ul>
         </div>
         <div className="bg-yellow-400 p-4 rounded-lg shadow-md">
@@ -33,7 +25,7 @@ const Chess: React.FC = () => {
             <li>opening theory</li>
             <li>end game strategy</li>
             <li>middle game tactics</li>
-            <li>rating 1500+</li>
+            <li>rating goal: 1500+</li>
           </ul>
         </div>
         <div className="bg-yellow-600 p-4 rounded-lg shadow-md text-white">
@@ -42,6 +34,7 @@ const Chess: React.FC = () => {
             <li>tournament games analysis</li>
             <li>grandmaster history and games</li>
             <li>advanced pattern recognition practices</li>
+            <li>rating goal: 2000+</li>
           </ul>
         </div>
       </div>
@@ -51,7 +44,7 @@ const Chess: React.FC = () => {
         <p>1 Lesson = $50</p>
         <p>10 Lesson Pack $400 (Save $100)</p>
       </div>
-      <div className="text-center mt-8">
+      <div className="text-center my-8">
         <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
           Schedule Your First Lesson
         </button>
